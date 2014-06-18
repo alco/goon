d0f7d443502116219fb55e6421229b0345589754
Alexei Sholik <alcosholik@gmail.com>
2014-06-18 17:22:15 +0300

    Clean up proto_0_0

--- proto_0_0.go
+++ proto_0_0.go
@@ -6,19 +6,14 @@ import (
 )
 
 func proto_0_0(inFlag bool, outFlag, errFlag, workdir string, args []string) error {
-	done := make(chan bool)
-	done_count := 0
-
 	proc := exec.Command(args[0], args[1:]...)
 	proc.Dir = workdir
 
-		done_count += wrapStdin(proc, os.Stdin, inFlag, done)
-	/*if shouldWrapOut(*outFlag, *errFlag, "out", "err") {*/
-		done_count += wrapStdout(proc, os.Stdout, outFlag, done)
-	/*}*/
-	/*if shouldWrapOut(*errFlag, *outFlag, "err", "out") {*/
-		done_count += wrapStderr(proc, os.Stdout, os.Stderr, errFlag, done)
-	/*}*/
+	done := make(chan bool)
+	done_count := 0
+	done_count += wrapStdin(proc, os.Stdin, inFlag, done)
+	done_count += wrapStdout(proc, os.Stdout, outFlag, done)
+	done_count += wrapStderr(proc, os.Stdout, errFlag, done)
 
 	// Now we're ready to start the requested program
 	err := proc.Run()

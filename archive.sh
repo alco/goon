ff3a763bca89ff35a2f248274dd721602073f598
Alexei Sholik <alcosholik@gmail.com>
2014-06-20 01:25:31 +0300

    Fix archive script

--- archive.sh
+++ archive.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-mkdir -p archives
+set -e  # abort on the first error
+set -v  # display script source locations in error messages
+set -x  # print script steps
 
-root=../../bin/goon-xc/snapshot
-for dir in $(ls $root); do
-    tar -czf archives/goon_${dir}.tar.gz $root/$dir/goon*
+rm -rf archives
+mkdir archives
+
+gobin=$GOPATH/bin
+
+xcroot=$gobin/goon-xc/snapshot
+goonroot=$GOPATH/src/goon
+
+$gobin/goxc xc
+
+cd $xcroot
+for dir in *; do
+    cd $dir
+    tar -czf $goonroot/archives/goon_${dir}.tar.gz goon*
+    cd ..
 done
